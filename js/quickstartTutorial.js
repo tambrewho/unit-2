08c467b552fd9ba1f97114991ca32c1650847e77
Zoey <zcolglazier@wisc.edu>
2020-02-12 16:50:49 -0600

    update index.html scripts and quickstartTutorial.js

---
+++ js/quickstartTutorial.js
@@ -0,0 +1,49 @@
+//Leaflet Quick Start Guide
+var mymap = L.map('mapid').setView([51.505,-0.09], 13);
+
+// add mapbox tilelayer to map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoidGFtYnJld2hvIiwiYSI6ImNrNmp3ZDA2ZjAwbDAza28wemc2b3VlcW4ifQ.vEl-InkeyTW44vClE1RDFA'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+// adding circle outline to map
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+// adding polygon shape to map
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+// adding popups to previously added shapes activated by clicking
+marker.bindPopup("<strong>Hello world!</strong><br />I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+var popup = L.popup();
+
+// function for showing latitude-longitude coord when clicking map
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
