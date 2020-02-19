fb6acb552deef3e100f4d2b71529f2d3fdb751e0
wshujin <54903336+wshujin@users.noreply.github.com>
2020-02-19 17:56:09 -0600

    added updatePropSymbols
    Up to Step 10

--- js/main.js
+++ js/main.js
@@ -10,40 +10,204 @@ function createMap(){
         zoom: 2
     });
 
-    // DONE: BROWSE FOR NEW TILESET
     // added new OSM base tilelayer
-    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    	maxZoom: 17,
-    	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(mainmap);
 
     //call getData function
-    getData();
+    getData(mainmap);
 };
 
-// DONE: FIND NEW DATA
-//function to retrieve the data and place it on the map
-function getData(){
-    //load the data
-    $.getJSON("data/ActivityData.geojson", function(response){
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
 
-            //create a GeoJSON layer with circle markers and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(mainmap);
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
 
-        });
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        attributes.push(attribute);
+    };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
+};
+
+//Step 1: Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+
+    //Step 5: click listener for buttons
+    $('.step').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+
+    //Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();
+
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+
+    $('#panel').append('<button class="step" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="step" id="forward">Forward</button>');
+};
+
+
+//Step 2: Import GeoJSON data
+function getData(mainmap){
+  //load the data
+  $.getJSON("data/ActivityData.geojson", function(response){
+       //calculate minimum data value
+       minValue = calcMinValue(response);
+
+       //create an attributes array
+       var attributes = processData(response);
+       //call function to create proportional symbols
+       createPropSymbols(response, attributes);
+       createSequenceControls(attributes);
+  });
+};
+
+//function to convert markers to circle markers
+function pointToLayer(feature, coordinates, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    //check
+    console.log(attribute);
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(coordinates, options);
+
+    //build popup content string
+    var popupContent = "<p><b>City:</b> " + feature.properties.CountryName + "</p>"
+                                + "<p><b>Year:</b> " + attribute + "</p>";
+    //add formatted attribute to popup content string
+    popupContent += "<p><b>Rural population growth (annual %):</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker with an offset
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+  //create a Leaflet GeoJSON layer and add it to the map
+  L.geoJson(data, {
+      pointToLayer: function(feature, coordinates){
+        return pointToLayer(feature, coordinates, attributes);
+      }
+  }).addTo(mainmap);
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    mainmap.eachLayer(function(layer){
+      if (layer.feature && layer.feature.properties[attribute]){
+          //access feature properties
+          var props = layer.feature.properties;
+
+          //update each feature's radius based on new attribute values
+          var radius = calcPropRadius(props[attribute]);
+          layer.setRadius(radius);
+
+          //build popup content string
+          var popupContent = "<p><b>City:</b> " + props.CountryName + "</p>"
+                                      + "<p><b>Year:</b> " + attribute + "</p>";
+
+          //add formatted attribute to popup content string
+          popupContent += "<p><b>Rural population growth (annual %):</b> " + props[attribute] + "</p>";
+
+          //update popup content
+          popup = layer.getPopup();
+          popup.setContent(popupContent).update();
+      };
+    });
+};
+
+function calcMinValue(data){
+     //create empty array to store all data values
+     var allValues = [];
+
+     //loop through each city
+     for(var city of data.features){
+          //loop through each year
+          for(var year = 2008; year <= 2018; year+=1){
+                //get population for current year
+               var value = city.properties[String(year)];
+               //add value to array
+               allValues.push(value);
+           }
+     }
+     //get minimum value of our array
+     var minValue = Math.min(...allValues)
+
+     return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+     //constant factor adjusts symbol sizes evenly
+     var minRadius = 20;
+     //Flannery Appearance Compensation formula
+     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+     return radius;
 };
 
 $(document).ready(createMap);
